6e4183596fc0e0103b317e69e9dd30a5672fdca9
SANTIAGO VARELA JIM�NEZ <santiagovarela314184@correo.itm.edu.co>
2023-04-02 13:51:05 -0500

    I have created the DAL folder with its Entity named Ticket

--- ConcertTicketSystem/ConcertTicketSystem/DAL/Entity/Ticket.cs
+++ ConcertTicketSystem/ConcertTicketSystem/DAL/Entity/Ticket.cs
@@ -2,5 +2,16 @@
 {
     public class Ticket
     {
+        [Key]
+        [Required]
+        public Guid Id { get; set; }
+
+        [Display(Name = "Fecha de ingreso")]
+        public DateTime? UserDate { get; set; } //Era Use de uso no "User"
+
+        [Display(Name = "Estado")]
+        public bool IsUsed { get; set; }
+        [Display(Name = "Porteria de ingreso")]
+        public string? EntranceGate { get; set; }
     }
 }
